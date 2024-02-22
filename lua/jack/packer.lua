fe5eed8463945195af7a9e6b766dcada729b70fa
Luis Sastré <luis.sastre@nubank.com.br>
2024-02-21 18:43:16 -0600

    Adding more configs to the editor

--- lua/jack/packer.lua
+++ lua/jack/packer.lua
@@ -50,4 +50,23 @@ return require('packer').startup(function(use)
     }
   }
 
+  -- Clojure
+  use('tpope/vim-dispatch')
+  use('clojure-vim/vim-jack-in')
+  use('radenling/vim-dispatch-neovim')
+  use('Olical/conjure')
+
+  use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
+
+  use("lukas-reineke/lsp-format.nvim")
+
 end)
--- lua/jack/remap.lua
+++ lua/jack/remap.lua
@@ -1,3 +1,19 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local maximized = false
+function ToggleMaximize()
+  if maximized then
+    vim.cmd(":execute 'wincmd ='")
+  else
+    vim.cmd(':vertical resize +999')
+    vim.cmd(':resize +999')
+  end
+  maximized = not maximized
+end
+
+vim.keymap.set("n", "<C-w>z", "<cmd>lua ToggleMaximize() <CR>", {noremap = true, silent = true})
+
+vim.keymap.set("n", "<leader>.", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
