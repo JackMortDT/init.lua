91b95d6a0f4cc6052fa8a0358aa5fac033844e6e
LuisSastré <soul6496@gmail.com>
2024-02-25 15:06:03 -0600

    Adding configuration for completition

--- after/plugin/vim_mix_format.lua
+++ after/plugin/vim_mix_format.lua
@@ -1,5 +1,6 @@
 require("mix").setup()
 vim.g.mix_format_on_save = 1
 
-vim.api.nvim_set_keymap('n', '<Leader>em', ':!mix test<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>ef', ':!mix test %<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ea', ':!mix test<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>et', ':!mix test %<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ec', ":execute '!mix test %:' . line('.')<CR>", { noremap = true, silent = true })
