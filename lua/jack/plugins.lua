0cc7dfbb3727ab649d78f90fe52326ffacc651db
LuisSastré <soul6496@gmail.com>
2024-03-10 00:05:24 -0600

    Adding configuration for the lsp

--- lua/jack/plugins.lua
+++ lua/jack/plugins.lua
@@ -1,15 +1,20 @@
 local M = {}
 
 M.setup = {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-  },
+  -- File plugins
+  {"nvim-lua/plenary.nvim"},
+  {"nvim-tree/nvim-web-devicons"},
+  {"MunifTanjim/nui.nvim"},
+  {"nvim-neo-tree/neo-tree.nvim", branch = "v3.x"},
+
+  -- Lsp plugins
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
 }
 
 return M
