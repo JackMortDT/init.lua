c7682e03876baf3a66a01de3df15e2b213e0a80d
LuisSastré <soul6496@gmail.com>
2024-03-09 23:02:00 -0600

    Adding plugin manager and neotree plugin

--- lua/jack/init.lua
+++ lua/jack/init.lua
@@ -1,1 +1,33 @@
-print("Hello from jack folder!")
+vim.g.mapleader = " "
+
+-- Package mnanagment with lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    },
+    config = function()
+      require("neo-tree").setup()
+    end,
+  },
+})
