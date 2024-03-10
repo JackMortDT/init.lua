a82c5c6e0f950cfbe36dee0b744c2c7d8bcc5100
LuisSastré <soul6496@gmail.com>
2024-03-10 11:37:12 -0600

    Refactor on plugins stuff

--- lua/jack/plugins.lua
+++ lua/jack/plugins.lua
@@ -1,27 +1,40 @@
-local M = {}
+local plugins = {}
 
-M.setup = {
+plugins.setup = {
   -- Code editor
-  {"rose-pine/neovim", name = "rose-pine"},
-  {"folke/neodev.nvim"},
+  { "rose-pine/neovim",                 name = "rose-pine" },
+  { "folke/neodev.nvim" },
 
   -- File plugins
-  {"nvim-lua/plenary.nvim"},
-  {"nvim-tree/nvim-web-devicons"},
-  {"MunifTanjim/nui.nvim"},
-  {"nvim-neo-tree/neo-tree.nvim", branch = "v3.x"},
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
+  { "MunifTanjim/nui.nvim" },
+  { "nvim-neo-tree/neo-tree.nvim",      branch = "v3.x" },
 
   -- Lsp plugins
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'L3MON4D3/LuaSnip'},
+  { 'williamboman/mason.nvim' },
+  { 'williamboman/mason-lspconfig.nvim' },
+  { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+  { 'neovim/nvim-lspconfig' },
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/nvim-cmp' },
+  { 'L3MON4D3/LuaSnip' },
 
   -- Clojure
-  {"Olical/conjure"}
+  { "Olical/conjure" }
 }
 
-return M
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins.setup)
